d7a5113030e035ee36ea21f7ac4de11be2d8bb2c
timmy <awoyeyetimilehin@gmail.com>
2024-01-17 06:44:05 +0100

    Remove ignored files

--- target/classes/db/migration/V1__Initial.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE book (
-                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                      title VARCHAR(255),
-                      author VARCHAR(255),
-                      publication_year DATE,
-                      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                      updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
